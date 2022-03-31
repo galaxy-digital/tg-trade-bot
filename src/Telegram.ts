21920ec1a6657df84167a807f849eafee6288826
galaxy126 <galaxy85126@gmail.com>
2022-03-31 07:26:49 +0100

    fix

--- src/Telegram.ts
+++ src/Telegram.ts
@@ -1,13 +1,16 @@
 require("dotenv").config()
+const isDev = process.env.NODE_ENV==="development"
 import * as express from 'express'
 import * as fs from 'fs'
+import axios from 'axios'
 import { setlog } from './helper'
 import { Config, getOrCreateUser, Users } from './Model'
 const router = express.Router()
 
 const now = () => Math.round(new Date().getTime()/1000)
 
-
+const apiUrl = isDev ? 'https://api.telegram.org' : 'https://api.telegram.org'
+const botKey = (isDev ? process.env.DEV_BOT_KEY : process.env.BOT_KEY) || ''
 
 export const initTelegram = async () => {
 
@@ -22,83 +25,47 @@ export const pushMessage = (chatId:string, text:string) => {
 }
 
 export const replyImage = async (replyToken:string, uri:string) => {
-	/* 
-	https://api.telegram.org/bot1183414296:AAH6UESAFVW81Bto07BZtFfSAZQcLRnF3tA/setWebhook
-	{
-		"url": "https://bac27031.ngrok.io/telegram/7zhTchRHHrHHgTxmQMx5tFpuREpVGJ4q"
-	}
-	Content-Type : application/json 
-	*/
+	
 }
 
-router.post("/add/webhook", (req:express.Request, res:express.Response)=>{
-
+router.post("/set-webhook", async (req:express.Request, res:express.Response)=>{
+	try {
+		const { url } = req.body
+		const response = await axios.post(`${apiUrl}/bot${botKey}/setWebhook`, { url: url + "/api/telegram/webhook" })
+		res.json(response.data)
+	} catch (error) {
+		res.json(error)
+	}
 })
 
 router.post("/webhook", (req:express.Request, res:express.Response)=>{
-	fs.appendFileSync(__dirname + '/../response.json', JSON.stringify(req.body, null, '\t') + '\r\n\r\n')
-	/* 
-	if (req.body.events && req.body.events.length!==0) {
-		const event = req.body.events[0]
-		const { message, source } = event
-		handleWebHook(event, source, message)
-	} 
-	*/
+	try {
+		// fs.appendFileSync(__dirname + '/../response.json', JSON.stringify(req.body, null, '\t') + '\r\n\r\n')
+		if (req.body.message!==undefined)  {
+			parseMessage(req.body.message)
+		} else if (req.body.channel_post!==undefined)  {
+		} else if (req.body.my_chat_member!==undefined)  {
+		}
+	} catch (error) {
+		setlog('webhook', error)
+	}
 	res.status(200).send('')
 })
 
-const handleWebHook = async ():Promise<boolean> => {
+const parseMessage = async (message:any):Promise<boolean> => {
 	try {
-		
-		/* const replyToken = event.replyToken
-		const p = message.text.indexOf(' ')
-		let cmd = '', params = ''
-		if (p===-1) {
-			cmd = message.text.trim()
-		} else {
-			cmd = message.text.slice(0, p).trim()
-			params = message.text.slice(p + 1).trim()
+		const { from, chat, date, forward_from, left_chat_member, text } = message
+
+		const valid = from!==undefined && chat!==undefined && text!==undefined && forward_from===undefined
+
+		if (valid) {
+			
 		}
-		if (isAdmin(source.userId)) {
-			const result = await parseAdminCommand(source.groupId || '', replyToken, cmd, params)
-			if (result===true) return true
-		} */
-		// return await parseCommand(source.groupId || '', source.userId, replyToken, cmd, params)
-	} catch (error) {
-		console.log(error)
-	}
-	return false
-}
-
-const parseAdminCommand = async (groupId:string, replyToken:string, cmd:string, param:string):Promise<boolean> => {
-	try {
-		/* switch (cmd) {
-		case AdminCommands.start:
-			{
-				if (currentRound.roundId!==0) {
-					await replyMessage(0, replyToken, ERROR_ALREADY_STARTED.replace('{roundId}', String(currentRound.roundId)))
-					return false
-				}
-				await startRound()
-				await replyMessage(0, replyToken, MSG_STARTED.replace('{roundId}', String(currentRound.roundId)))
-			}
-			break
-		default: 
-			return false
-		}
-		return true */
-	} catch (error) {
-		setlog("parseAdminCommand", error)
-		/* await replyMessage(null, replyToken, ERROR_UNKNOWN_ERROR) */
-	}
-	return false
-}
 
 
-const parseCommand = async (groupId:string, userId:string, replyToken:string, cmd:string, param:string):Promise<boolean> => {
-	try {
+
 		// if (groupId!=='') await insertGroupId(groupId)
-		const user = await getOrCreateUser(userId)
+		// const user = await getOrCreateUser(userId)
 		// const uid = user.id
 		
 		/* switch (cmd) {
